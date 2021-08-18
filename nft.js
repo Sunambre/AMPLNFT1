8e8ef8c6d2a6b992e60b4362cad2a50129b8ec75
Sunambre <89150407+Sunambre@users.noreply.github.com>
2021-08-18 15:24:27 -0500

    Update nft.js

--- nft.js
+++ nft.js
@@ -45,12 +45,12 @@ let targetPrice = 0;
 function checkMarket() {
   
   fetch("https://web-api.ampleforth.org/eth/token-info").then(async response => {
-    const json = await response.json()
+    const json = await response.json();
     targetPrice = json.lastRebaseInfo.targetRateAtRebase;
 
     fetch("https://api.coingecko.com/api/v3/simple/price?ids=ampleforth&vs_currencies=usd").then(async response => {
-       const json = await response.json()
-       document.getElementById("price_slider_output").value = parseFloat(json.ampleforth.usd).toFixed(2)
+       const json = await response.json();
+       document.getElementById("price_slider_output").value = parseFloat(json.ampleforth.usd).toFixed(2);
        document.getElementById("price_slider").value = (parseFloat(json.ampleforth.usd)* 100 - 100).toFixed(2);
 
        loadGif(json.ampleforth.usd,targetPrice)
@@ -62,7 +62,7 @@ function checkMarket() {
 document.getElementById("price_slider").addEventListener("input", e => {
   const marketRate = parseFloat((1 + (e.target.value / 100)).toFixed(2));
   document.getElementById("price_slider_output").value = marketRate;
-  loadGif(marketRate,targetPrice) 
+  loadGif(marketRate,targetPrice) ;
 })
              
 
@@ -70,4 +70,4 @@ document.getElementById("check_market_button").addEventListener("click", e => {
   checkMarket();
 })
 
-checkMarket()
+checkMarket();
