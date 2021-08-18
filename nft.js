706b62ab9795cb14e3e826c0fe54d3b0603c449a
Sunambre <89150407+Sunambre@users.noreply.github.com>
2021-08-18 14:13:06 -0500

    Update nft.js

--- nft.js
+++ nft.js
@@ -1,51 +1,73 @@
-function loadGif(marketRate) {
+function loadGif(marketRate,targetPrice) {
+  
+  let upA = targetPrice * 1.05;
+  let upB = targetPrice * 1.25;
+  let upC = targetPrice * 1.5;
+
+  let downA = targetPrice * 0.95;
+  let downB = targetPrice * 0.75;
+  let downC = targetPrice * 0.5;
   
   const gifHolder = document.getElementById("gifDisplay");
   
-  if(marketRate > 1.7)
+  if(marketRate > upC)
   {
   	gifHolder.src = "https://i.imgur.com/gzj9Ex1.gif";
   }
-  if((marketRate <= 1.7)&&(marketRate >= 1.2))
+  if((marketRate <= upC)&&(marketRate >= upB))
   {
     gifHolder.src = "https://i.imgur.com/RaxHT8K.gif";
   }
-  if((marketRate < 1.2)&&(marketRate >= 1.07))
+  if((marketRate < upB)&&(marketRate >= upA))
   {
     gifHolder.src = "https://i.imgur.com/eL6tzb9.gif";
   }
-  if((marketRate < 1.07)&&(marketRate >= 0.98))
+  if((marketRate < upA)&&(marketRate >= downA))
   {
     gifHolder.src = "https://i.imgur.com/fqaxE9O.gif";
   }
-  if((marketRate < 0.98)&&(marketRate >= 0.89))
+  if((marketRate < downA)&&(marketRate >= downB))
   {
     gifHolder.src = "https://i.imgur.com/PFPhQ2s.gif";
   }
-  if((marketRate < 0.89)&&(marketRate >= 0.69))
+  if((marketRate < downB)&&(marketRate >= downC))
   {
     gifHolder.src = "https://i.imgur.com/sH6TD6A.gif";
   }
-  if((marketRate < 0.69)&&(marketRate >= 0))
+  if((marketRate < downC)&&(marketRate >= 0))
   {
     gifHolder.src = "https://i.imgur.com/ucbClGR.gif";
   }
 }
 
+let targetPrice = 0;
+
+function checkMarket() {
+  
+  fetch("https://web-api.ampleforth.org/eth/token-info").then(async response => {
+    const json = await response.json()
+    targetPrice = json.lastRebaseInfo.targetRateAtRebase;
+
+     fetch("https://api.coingecko.com/api/v3/simple/price?ids=ampleforth&vs_currencies=usd").then(async response => {
+       const json = await response.json()
+       document.getElementById("price_slider_output").value = parseFloat(json.ampleforth.usd).toFixed(2)
+       document.getElementById("price_slider").value = (parseFloat(json.ampleforth.usd)* 100 - 100).toFixed(2);
+
+       loadGif(json.ampleforth.usd,targetPrice)
+     })
+  })
+ 
+}
+
 document.getElementById("price_slider").addEventListener("input", e => {
   const marketRate = parseFloat((1 + (e.target.value / 100)).toFixed(2));
-  document.getElementById("price_slider_output").value = marketRate
-  loadGif(marketRate) 
+  document.getElementById("price_slider_output").value = marketRate;
+  loadGif(marketRate,targetPrice) 
 })
              
 
-
-fetch("https://api.coingecko.com/api/v3/simple/price?ids=ampleforth&vs_currencies=usd").then(async response => {
-  const json = await response.json()
-
-  document.getElementById("price_slider_output").value = json.ampleforth.usd
-  
-  loadGif(json.ampleforth.usd)
+document.getElementById("check_market_button").addEventListener("click", e => {
+	checkMarket();
 })
 
-
+checkMarket()
